ec2cef5d978c632af97d8c33de2b86014406dd0a
unknown <prabhusharmila.t@gmail.com>
2023-05-05 20:47:51 -0400

    Frontend

---
+++ src/components/CreateComponent.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <h3 class="text-center">Create Student</h3>
+            <form @submit.prevent="handleSubmitForm">
+            <div class="form-group">
+                    <label>ID</label>
+                    <input type="text" class="form-control" v-model="student.id" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" class="form-control" v-model="student.name" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" class="form-control" v-model="student.email" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Phone</label>
+                    <input type="text" class="form-control" v-model="student.phone" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Department</label>
+                    <input type="text" class="form-control" v-model="student.department" required>
+                </div>
+
+                <br/>
+                <div class="form-group">
+                    <button class="btn btn-danger btn-block">Create</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        data() {
+            return {
+                student: {
+                    id:'',
+                   name: '',
+                   email: '',
+                   phone: '',
+                   department:''
+                }
+            }
+        },
+        methods: {
+             handleSubmitForm() {
+                
+                let apiURL = 'http://localhost:8080/api/create-student';
+                
+                axios.post(apiURL, this.student).then(() => {
+                  this.$router.push('/view')
+                  this.student = {
+                    id:'',
+                    name: '',
+                    email: '',
+                    phone: '',
+                    department:''
+                  }
+                }).catch(error => {
+                    console.log(error)
+                });
+            }
+        }
+    }
+</script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,397 @@
+<template>
+
+    <body>
+
+        <div class="container" id="navmenu">
+
+        <nav> 
+   
+      
+       <h1> Portfolio </h1>
+
+        </nav>
+
+        
+
+        <div class="sidebar">
+
+            
+        <img class="profile" src="../assets/profile.jpg" width="300px" height="200px"> 
+            
+
+        <h1>Prabhusharmila Tummalapalli</h1>
+        <h4><u>Email:</u>prabhu23@gmail.com</h4>
+        <h4><u>Contact Number:</u>+1 - 010 - 234 - 4567</h4>
+        <h4><u>LinkedIN:</u><a href="https://www.linkedin.com/in/prabhu/">https://www.linkedin.com/in/prabhu/</a> </h4>
+
+
+            <h1 id="black" style="padding-top: 50px;"><u>Profile</u></h1>
+           
+            <p id="black">Self-driven and diligent Business Analytics - concentration in Global Supply Chain student with 
+                experience in IT- corporate field and good knowledge of statistic analytics, visualization tools and 
+                concepts along with supply chain models.  Had an educational background of 4+ years in Computer Science and 
+                ability to communicate effectively in group and interpersonal settings by managing work-place stress to 
+                provide quality and meet deadlines, currently looking for an developer/Consultant position to gain
+                practical knowledge by applying skills that I presently possess. 
+                </p>
+            
+
+        </div>
+
+
+        <div id="content1">
+
+            <h1><u>Education</u></h1>
+
+            <table>
+                <tr>
+                    <td>University of New Haven</td>
+                    <td>Masters in Computer Science</td>
+                    <td>3.9/4</td>
+                </tr>
+
+                <tr>
+                    <td>University of Pennyslvania</td>
+                    <td>Bachelors in Computer Science</td>
+                    <td>3.8/4</td>
+                </tr>
+
+                <tr>
+                    <td>University of Dayton</td>
+                    <td>High School</td>
+                    <td>3.7/4</td>
+                </tr>
+
+            </table>
+
+            </div>
+
+              
+
+
+
+                <div id="content2">
+
+                <h1><u>Experience</u></h1>
+                <h3>Tata Consultancy Services 2019-2022</h3>
+                <h5>Software Engineer</h5>
+                <ol>
+                    <li>Communicate with customers via phone, email and social media</li>
+                    <li>Respond promptly to customer complaints and questions</li>
+                    <li>Give customers information about product and services</li>
+                    <li>Process orders, forms, applications and requests </li>
+                    <li>Maintain a positive and professional attitude towards customers.</li>
+                </ol>
+
+                <h3>Wipro Private Limited   2022-2023</h3>
+                <h5>Senior Software Engineer</h5>
+                <ol>
+                    <li>Create a wide varieties of delivarables using Adobe</li>
+                    <li>Work with creative team to brainstrom innovative advertising campaigns</li>
+                    <li>Resize images using Adobe</li>
+                    <li>Complete minor HTML/CSS coding as needed</li>
+                    <li>Research emerging design trends</li>
+                </ol>
+
+                </div>
+
+                
+
+        
+
+
+                <div id="content3">
+                    <h1><u>Blog</u></h1>
+                    <ol>
+                        <li><a href="https://www.sharmilaprabhu.webdesigns.com">https://www.sharmilaprabhu.webdesigns.com</a></li>
+                        <li><a href="https://www.prabhu.appdev.com">https://www.prabhu.appdev.com</a></li>
+                        <li><a href="https://www.sharmila.clouddev.com">https://www.sharmila.clouddev.com</a></li>
+                        <li><a href="https://www.sharmila/reporting.com">https://www.sharmila/reporting.com</a></li>
+                    </ol>
+                    
+                </div>
+
+                <div id="content4">
+
+                <h1><u>Technical Skills</u></h1>
+                <h5>Programming Languages: C, C++, Java</h5>
+                <h5>Reporting: Microsoft Power BI, Tableau</h5>
+                <h5>Database: SQL</h5>
+                <h5>Cloud Technologies: Azure, AWS</h5>
+                <h5>Web Technologies: HTML, CSS</h5>      
+
+
+            </div>
+
+                <div id="content5">
+
+                <h1><u>Certifications</u></h1>
+                <ol>
+                    <li>DA - 100 Analyzing data with Power BI</li>
+                    <li>Exam 70-49 Designing database for Microsoft SQL Server</li>
+                    <li>DP-201 Designing Azure Data Solution</li>
+                    <li>AZ-400 Designing and Implementing Microsoft DevOps Solutions</li>
+                    <li>AI-102 Designing and Implementing a Microsoft AI Solution</li>
+                </ol>
+
+                
+
+            </div>    
+                
+
+                
+                <footer>
+
+                <h5>@Copyright 2025 ResumeCreate All Rights Reserved</h5>
+
+
+                </footer>
+
+        </div>
+
+
+    </body>
+</template>
+
+<style scoped>
+.container{
+    font-family:'Noto Serif', serif;
+    font-size: 1em;  
+    display: grid;
+    height: 100vh;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-areas: "nav nav nav nav nav nav" 
+    "sidebar content1 content2 content2 content2 content2" "sidebar content3 content4 content4 content4 content4"
+    "sidebar content5 content5 content5 content5 content5"
+    "footer footer footer footer footer footer";
+    grid-gap:0.5rem;
+    color: white;
+    
+}
+
+.sidebar{
+
+    background:chocolate;
+    border-radius: 5px;
+    padding: 5px;
+    grid-area: sidebar;
+    color: white;
+    font-weight: bold;
+    padding: 10px;
+    width: 500px;
+    
+}
+
+#content1{
+    background:yellow;
+    border-radius: 5px;
+    padding: 5px;
+    grid-area: content1;
+    color:black;
+    font-weight: bold;
+    
+}
+
+#content2{
+    background:crimson;
+    border-radius: 5px;
+    padding: 5px;
+    grid-area: content2;
+    color: white;
+    font-weight: bold;
+   
+    
+}
+
+#content3{
+    background:darkblue;
+    border-radius: 5px;
+    padding: 5px;
+    grid-area: content3;
+    color: white;
+    font-weight: bold;
+   
+    
+}
+
+#content4{
+    background:darkolivegreen;
+    border-radius: 5px;
+    padding: 5px;
+    grid-area: content4;
+    color: white;
+    font-weight: bold;
+  
+    
+}
+
+nav{
+    
+    padding:10px 10px;
+    display:block;
+    border: 1px solid;
+    background-color:khaki;
+    font-family: 'Noto Serif' serif;
+    border-radius: 5px;
+    grid-area: nav;
+    height: 60px;
+    overflow: hidden;
+    text-align:center;
+    color:black;
+    
+}
+
+#content5{
+    background:orange;
+    border-radius: 5px;
+    padding: 5px;
+    grid-area: content5;
+    color: white;
+    font-weight: bold;
+    
+}
+
+body{
+    background-color:azure;
+    
+}
+
+
+#black{
+    color: black;
+    
+    
+}
+
+.profile{
+    float:right;
+}
+
+table{
+    border: 1px solid black;
+    border-spacing: 5px;
+    font-family: 'Noto Serif' serif;
+}
+
+
+.fa-instagram {
+    background-image:linear-gradient(#fa7e1e,#8a3ab9,#bc2a8d,#bc2a8d,#fccc63,#cd486b,#cd486b);
+    color: white;
+}
+.fa-facebook {
+    background: #3B5998;
+     color: white;
+}
+.fa-twitter {
+      background: #55ACEE;
+      color: white;
+}
+.fa-linkedin {
+        background: #007bb5;
+        color: white;
+}
+.fa {
+        padding: 20px;
+        font-size: 30px;
+        width: 30px;
+        text-align: center;
+        text-decoration: none;
+        margin: 5px 2px;
+        border-radius: 50%;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+footer{
+    background:saddlebrown;
+    border-radius: 5px;
+    padding: 5px;
+    grid-area: footer;
+    color: white;
+    font-weight: bold;
+    text-align: center;
+    
+}
+
+
+.about{
+    background-color:sandybrown;
+    font-family: 'Noto Serif' serif;
+    display: grid;
+    grid-area: about;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr;
+    grid-template-areas: "about about about about";
+    font-size: 1em;
+    grid-gap: 0.5rem;
+    height: 100px;
+    
+
+}
+
+.aboutme{
+    text-align: center;
+    font-size: 50px;
+    font-family: 'Noto Serif' serif;
+    
+}
+
+#contact{
+    background-color: mistyrose;
+    text-align: center;
+    font-family: 'Noto Serif' serif;
+
+}
+
+.container .icon{
+    display: none;
+}
+
+@media only screen and (max-width:500px){
+
+    .container a:not(:first-child){
+        display: none;
+    }
+
+    .container a.icon{
+        float:right;
+        display: block;
+        float: right;
+    }
+
+    .container.nav.responsive a{
+        position: relative;
+        float:none;
+        display: block;
+        text-align: left;
+        right: 0;
+        top:0;
+    }
+    .container.nav.responsive .icon{
+        position:absolute;
+    }
+
+    .container{
+        grid-template-columns:1fr;
+        grid-template-rows: 0.4fr 1.5fr 0.5fr 0.5fr 0.5fr 1fr;
+        grid-template-areas: 
+        "nav"
+        "main"
+        "sidebar"
+        "content1"
+        "content2"
+        "content3"
+        "content4"
+        "content5"
+        "footer";
+
+    }
+
+    
+
+}
+</style>
+
